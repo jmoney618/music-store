16f4490b62be6b1e9e257da101c06e3659cc7d83
Jose Q <jquinones618@gmail.com>
2016-02-07 01:32:43 -0500

    updated

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jose's Music Store</title>
+    <link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
+	<link type="text/css" rel="stylesheet" href="styles.css"/>
+</head>
+<body>
+	<div class="container">
+		<header>
+			<?php include("menu.php") ?>
+		</header>
+
+        <section>
+            <p>Welcome to Jose's Music Store! Here you'll find all of your favorites from modern hits to the classics we all know and love.</p>
+			<p><a href="selection.php">Now go buy some music!</a></p>
+        </section>
+    </div>
+</body>
+</html>
